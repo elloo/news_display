376983e60124dbfa2838c45868e69c7d132d859e
Ewe Lin Loo <ewelinloo@gmail.com>
2019-09-24 17:02:59 +1000

    Fix TypeError bug

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
-function generateURL(){
+function generateURL(query, event){
+    event.preventDefault();
+    if (query != ""){
+        articleFetch(`https://newsapi.org/v2/everything?q=${query}&apiKey=81e2959a083d42e091898d3be711e743`);
+    } else {
+        articleFetch("https://newsapi.org/v2/top-headlines?country=au&apiKey=81e2959a083d42e091898d3be711e743");
 
+    } 
 }
 
 function displayArticle(article){
@@ -22,27 +28,24 @@ function displayArticle(article){
         desc.innerHTML = `${article.content}` + more;
     }
     mainDiv.appendChild(desc);
-
-    console.log(article.urlToImage);
 }
 
 async function articleFetch(url) {
-try {
-    const response = await fetch(url);
-    const jsonRes = await response.json();
-    const titlesUl = document.querySelector("#titles");
-    for (var i = 0; i < 5; i++){
-        let newLink = document.createElement("li");
-        let articleTitle = `${jsonRes.articles[i].title}`;
-        newLink.setAttribute("onclick", `displayArticle(${JSON.stringify(jsonRes.articles[i])})`)
-        newLink.innerHTML = articleTitle;
-        titlesUl.appendChild(newLink);
+    try {
+        const response = await fetch(url);
+        console.log(response);
+        const jsonRes = await response.json();
+        const titlesUl = document.querySelector("#titles");
+        for (var i = 0; i < 5; i++){
+            let newLink = document.createElement("li");
+            let articleTitle = `${jsonRes.articles[i].title}`;
+            newLink.setAttribute("onclick", `displayArticle(${JSON.stringify(jsonRes.articles[i])})`)
+            newLink.innerHTML = articleTitle;
+            titlesUl.appendChild(newLink);
+        }
+        return jsonRes.articles;
     }
-    return jsonRes.articles;
+    catch (err) {
+        console.log('!Fetch failed!', err);
+    }
 }
-catch (err) {
-    console.log('Fetch failed', err);
-}
-}
-
-articleFetch("https://newsapi.org/v2/top-headlines?country=au&apiKey=81e2959a083d42e091898d3be711e743");
