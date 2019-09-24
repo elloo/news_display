dd5ea97610c16e8f12f47a1988cfef984048547b
Ewe Lin Loo <ewelinloo@gmail.com>
2019-09-24 11:15:28 +1000

    Add onclick function

--- app.js
+++ app.js
@@ -1,5 +1,25 @@
-function displayArticle(id){
-    console.log(id);
+function displayArticle(article){
+    const mainDiv = document.querySelector("#main");
+    mainDiv.innerHTML = "";
+
+    let img = document.createElement("img");
+    if (article.urlToImage.startsWith("/")){
+        img.setAttribute("src", "https://content.thriveglobal.com/wp-content/uploads/2018/03/news-1.jpg");
+    } else {
+        img.setAttribute("src", `${article.urlToImage}`);
+    }
+    mainDiv.appendChild(img);
+
+    let desc = document.createElement("p");
+    let more = ` <a href=${article.url}>[More]</a>`;
+    if (`${article.description}` != ""){
+        desc.innerHTML = `${article.description}` + more;
+    } else {
+        desc.innerHTML = `${article.content}` + more;
+    }
+    mainDiv.appendChild(desc);
+
+    console.log(article.urlToImage);
 }
 
 async function logFetch(url) {
@@ -10,16 +30,15 @@ try {
     for (var i = 0; i < 5; i++){
         let newLink = document.createElement("li");
         let articleTitle = `${jsonRes.articles[i].title}`;
-        newLink.setAttribute("id", i);
-        newLink.setAttribute("onclick", "displayArticle(this.id)")
+        newLink.setAttribute("onclick", `displayArticle(${JSON.stringify(jsonRes.articles[i])})`)
         newLink.innerHTML = articleTitle;
         titlesUl.appendChild(newLink);
     }
-
+    return jsonRes.articles;
 }
 catch (err) {
     console.log('Fetch failed', err);
 }
 }
 
-logFetch('https://newsapi.org/v2/top-headlines?country=au&apiKey=81e2959a083d42e091898d3be711e743')
+var articles = logFetch('https://newsapi.org/v2/top-headlines?country=au&apiKey=81e2959a083d42e091898d3be711e743');
